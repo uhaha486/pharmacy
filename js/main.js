51b120b8929586467961a44c75f956b856a876af
uhaha486 <52910600+uhaha486@users.noreply.github.com>
2019-08-14 18:07:47 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// window.onscroll = function () { myFunction() };
-
-// var header = document.getElementById("myHeader");
-// var sticky = header.offsetTop;
-
-// function myFunction() {
-//     if (window.pageYOffset > sticky) {
-//         header.classList.add("sticky");
-//     } else {
-//         header.classList.remove("sticky");
-//     }
-// }
 
 $('.depth1').hover(
     function () {
@@ -22,20 +10,61 @@ $('.depth1').hover(
 );
 
 
-$(document).ready(function () {
-    $('.slider').bxSlider(
-    {
-        auto:true,
-        speed:500
 
-    }
-    );
-    
+$(function(){
+    $('#group1').click(function(){
+        $('.notice-text').removeClass('show');
+        $('.news1').addClass('show');
+   
+    })
+    $('#group2').click(function(){
+        $('.notice-text').removeClass('show');
+        $('.news2').addClass('show');
+    })
+    $('#group3').click(function(){
+        $('.notice-text').removeClass('show');
+        $('.news3').addClass('show');
+    })
+})
+//tap color change
+
+$(function(){
+    $('#group1').click(function(){
+  $('button').removeClass('color');
+  $('#group1').addClass('color');
+    })
+    $('#group2').click(function(){
+        $('button').removeClass('color');
+        $('#group2').addClass('color');
+    })
+    $('#group3').click(function(){
+        $('button').removeClass('color');
+        $('#group3').addClass('color');
+    });
 });
 
 
 
 
+
+//sticky
+
+// window.onscroll = function () { myFunction() };
+
+// var header = document.getElementById("myHeader");
+// var sticky = header.offsetTop;
+// console.log(sticky);
+
+// function myFunction() {
+//     if (window.pageYOffset > sticky) {
+//         header.classList.add("sticky");
+//         $('#myHeader').css('backgrondColor', '#000');
+//         $('#myHeader .container img').attr('src', 'images/logo-white.png')
+//     } else {
+//         header.classList.remove("sticky");
+//         $('#myHeader .container img').attr('src', 'images/logo.png')
+//     }
+// }
 
 
 
